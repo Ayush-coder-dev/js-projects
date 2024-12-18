f664c98f72300df6e0c35e6136229e6790491a4a
ayush alekar <ayushalekar456@gmail.com>
2024-12-18 15:55:00 +0530

    almost coplete

--- first.js
+++ first.js
@@ -1,1 +1,11 @@
-console.log("ayush");
+const button = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+button.forEach((b)=>{
+    b.addEventListener("click",(e)=>{
+        if(e.target.id === "white"){
+            body.style.backgroundColor = e.target.id;
+
+    }})
+
+})
